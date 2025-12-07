80eca10c37a219344e6e27b85bb79f61f8a16375
Steven Winter <stevenwinter.me@gmail.com>
2025-12-06 19:09:35 -0500

    Railway port fix.

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
-app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:5001');
+const port = process.env.PORT || 5001; 
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
